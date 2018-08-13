ed6dc8897aaaea45efcfeeb5f668ef7c6b87fb26
hachimitu22 <hachimitu22.888@gmail.com>
2018-08-13 20:54:41 +0900

    **** 郵便番号の入力コンポーネントを作ろう

---
+++ react/books/imadoki-js-programmer/chapter03/section05/input_zip/src/ZipInput.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+export default class ZipInput extends Component {
+  constructor (props) {
+    super(props);
+    const v = (this.props.value) ? this.props.vale : '';
+
+    this.state = {
+      value: v,
+      isOK: this.checkValue(v)
+    };
+  }
+  checkValue (s) {
+    const zipPattern = /^\d{3}-\d{4}$/;
+    return zipPattern.test(s);
+  }
+  handleChange (e) {
+    const v = e.target.value;
+    const newValue = v.replace(/[^0-9-]+/g, '');
+    const newIsOK = this.checkValue(newValue);
+    this.setState({
+      value: newValue,
+      isOK: newIsOK
+    });
+  }
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      value: nextProps.value,
+      isOK: this.checkValue(nextProps.value)
+    });
+  }
+  render () {
+    const msg = this.renderStatusMessate();
+
+    return (
+      <div>
+        <label>郵便番号: <br />
+          <input type='text'
+            placeholder='郵便番号を入力'
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          />
+          {msg}
+        </label>
+      </div>
+    );
+  }
+  renderStatusMessate () {
+    const so = {
+      margin: '8px',
+      padding: '8px',
+      color: 'white'
+    };
+    let msg = null;
+    if (this.state.isOK) {
+      so.backgroundColor = 'green';
+      msg = <span style={so}>OK</span>;
+    } else {
+      if (this.state.value !== '') {
+        so.backgroundColor = 'red';
+        msg = <span style={so}>NG</span>;
+      }
+    }
+
+    return msg;
+  }
+}
