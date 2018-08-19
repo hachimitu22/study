d57e2f1ff004c5bf00e669f45bf1281c8e3f442e
hachimitu22 <hachimitu22.888@gmail.com>
2018-08-19 23:11:09 +0900

    **** プロジェクトを作成しよう

---
+++ react/books/imadoki-js-programmer/chapter05/section06/chat/src/styles.js
@@ -0,0 +1,17 @@
+export const styles = {
+  h1: {
+    backgroundColor: 'blue'
+  },
+  form: {
+    padding: 12
+  },
+  log: {
+    border: '1px solid silver'
+  },
+  name: {
+    border: '1px solid silver'
+  },
+  msg: {
+    border: '1px solid silver'
+  }
+};
