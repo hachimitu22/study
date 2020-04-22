a5109c32026620e1e70f85198990f5efb53c4ee4
hachimitu22 <hachimitu22.888@gmail.com>
2020-04-22 18:09:05 +0900

    CardGameManager が War 以外も扱えるように変更

--- uml/CardGame/project/ts/cardgame-manager.ts
+++ uml/CardGame/project/ts/cardgame-manager.ts
@@ -1,17 +1,19 @@
 import War from './war'
 import Keyboard from './keyboard'
 import CommandPrompt from './commandprompt'
+import CardGame from './cardgame';
 
 export default class CardGameManager {
   constructor(private input: Keyboard, private output: CommandPrompt) {
   }
   public run() {
-    let game: War;
+    let game: CardGame;
     while (game = this.selectGame()) {
       game.run();
+      break;
     }
   }
-  private selectGame(): War {
+  private selectGame(): CardGame {
     return new War(this.input, this.output);
   }
 }
